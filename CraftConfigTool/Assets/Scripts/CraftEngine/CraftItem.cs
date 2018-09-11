696ee5ec94373797f184541c17865a60d35e9d05
s_stolnikov <s_stolnikov@wargaming.net>
2018-09-11 18:09:41 +0300

    plugins import

---
+++ CraftConfigTool/Assets/Scripts/CraftEngine/CraftItem.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class CraftInfo
+{
+    public string item_id;
+    public int demand_count;
+}
+
+public interface IItemDescriptors
+{
+
+}
+
+[System.Serializable]
+public class UniqueItemDescriptors : IItemDescriptors
+{
+
+}
+
+[System.Serializable]
+public class PrizeBoneDiscountItemDescriptors : IItemDescriptors
+{
+
+}
+
+[System.Serializable]
+public class PrizeTankOpenItemDescriptors : IItemDescriptors
+{
+
+}
+
+[System.Serializable]
+public class TankUpgradeDescriptors : IItemDescriptors
+{
+
+}
+
+[System.Serializable]
+public class ChooseToCraftItemDescriptors : IItemDescriptors
+{
+
+}
+
+[System.Serializable]
+public class CraftDescriptors
+{
+    public UniqueItemDescriptors Unique = null;
+    public PrizeBoneDiscountItemDescriptors PrizeBoneDiscount = null;
+    public PrizeTankOpenItemDescriptors PrizeTankOpen = null;
+    public TankUpgradeDescriptors TankUpgrade = null;
+    public ChooseToCraftItemDescriptors ChooseToCraft = null;
+}
+
+[System.Serializable]
+public class CraftItem {
+    public string ItemTypeId;
+    public float CraftTime;
+    public CraftDescriptors Descriptors;
+    public CraftInfo[] CraftInfo;
+}
